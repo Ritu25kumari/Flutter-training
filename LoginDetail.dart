1296c9f41ff767355bb95140fabde0df0e934031
RITU <109500521+Ritu25kumari@users.noreply.github.com>
2023-08-28 16:06:58 +0530

    LoginDetail.dart

--- LoginDetail.dart
+++ LoginDetail.dart
@@ -1,22 +1,76 @@
 import 'package:flutter/material.dart';
-class LoginDetail extends StatelessWidget{
-  String name,email,phone,password;
+class DataModel {
+  String name, email, phone, password;
+  DataModel(
+      {required this.name, required this.email, required this.phone, required this.password});
 
+}
+class LoginDetails extends StatelessWidget{
+  final DataModel dataModel1;
+  LoginDetails({required this.dataModel1});
 
-  LoginDetail({required this.name, required this.email,required this.phone, required this.password});
-
+  @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     body: Column(
-       children: [
-         Text('Name: ${name}'),
-         Text('E-mail: ${email}'),
-         Text('Phone no.: ${phone}'),
-         Text('Password: ${password}'),
-       ],
-     )
-   );
+    return Scaffold(
+      appBar:AppBar(title: Text('User Detail'),),
+      body:
+      Column(
+        children: [
+          SizedBox(height: 20,),
+          Container(
+            height: 50,
+            width: 350,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+                side: BorderSide(color: Colors.deepPurple, width: 2)
+              ),
+              child: Text('  Name :${dataModel1.name}',
+              style: TextStyle(fontSize: 15),),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 350,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+                side: BorderSide(color: Colors.deepPurple,width: 2)
+              ),
+              child: Text('  Email :${dataModel1.email}',
+              style: TextStyle(fontSize: 15),),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 350,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+                side: BorderSide(color: Colors.deepPurple,width: 2)
+              ),
+              child: Text('  Phone number :${dataModel1.phone}',
+              style: TextStyle(fontSize: 15),),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 350,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.deepPurple, width: 2),
+                borderRadius: BorderRadius.circular(15)
+              ),
+              child: Text('  Password :${dataModel1.password}',
+              style: TextStyle(fontSize: 15),
+                ),
+            ),
+          )
+        ],
+      ),
+    );
   }
+
 }
 
 
