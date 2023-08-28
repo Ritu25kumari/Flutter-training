d69a87b71b2cb0a35bbf82b2cf9518bf8c115cf3
RITU <109500521+Ritu25kumari@users.noreply.github.com>
2023-08-28 16:05:47 +0530

    loginpage1.dart

--- loginpage1.dart
+++ loginpage1.dart
@@ -1,195 +1,216 @@
 import 'package:flutter/material.dart';
-import 'package:test/LoginDetail.dart';
+import 'LoginDetail.dart';
+
 
 void main(){
   runApp(LoginPage());
 }
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
+
+class LoginPage extends StatelessWidget {
+ @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: LoginApp (),
+     theme: ThemeData(primarySwatch: Colors.deepPurple),
+   );
+  }
 }
 
-class _LoginPageState extends State<LoginPage> {
-  TextEditingController _uname = new TextEditingController();
-  TextEditingController _email = new TextEditingController();
-  TextEditingController _phone = new TextEditingController();
-  TextEditingController _pass = new TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+class LoginApp extends StatefulWidget {
+  @override
+  State<LoginApp> createState() => _LoginAppState();
+}
+
+class _LoginAppState extends State<LoginApp>{
+   final TextEditingController _uname =  TextEditingController();
+   final TextEditingController _email =  TextEditingController();
+   final TextEditingController _phone = TextEditingController();
+   final TextEditingController _pass = TextEditingController();
+   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(title: Text('Login Form'),),
-        body: Center(
-          child: Container(
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(title: Text('Login Form'),),
+      body: Center(
+        child: Container(
             height: 600,
             width: 300,
             child: Form(
               key: _formKey,
               child:Column(children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: _uname,
-                  keyboardType: TextInputType.name,
-                  decoration: InputDecoration(
-                    labelText: 'User name',
-                    hintText: 'Enter Username',
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.orange,
-                            width: 2
-                        )
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _uname,
+                    keyboardType: TextInputType.name,
+                    decoration: InputDecoration(
+                      labelText: 'User name',
+                      hintText: 'Enter Username',
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.orange,
+                              width: 2
+                          )
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.deepPurple,
+                              width: 2
+                          )
+                      ),
+                      disabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.grey
+                          )
+                      ),
+                      prefixIcon: IconButton(
+                        icon: Icon(Icons.account_circle),
+                        onPressed: (){},
+                      ),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.deepPurple,
-                            width: 2
-                        )
-                    ),
-                    disabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.grey
-                        )
-                    ),
-                    prefixIcon: IconButton(
-                      icon: Icon(Icons.account_circle),
-                      onPressed: (){},
-                    ),
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return'Please enter your username';
+                      }
+                      return null;
+                    },
+
                   ),
-                  validator: (value){
-                    if(value!.isEmpty){
-                      return'Please enter your username';
-                    }
-                    return null;
-                  },
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: _email,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'email',
-                    hintText: 'Enter E-mail',
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.orange,
-                            width: 2
-                        )
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _email,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'email',
+                      hintText: 'Enter E-mail',
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.orange,
+                              width: 2
+                          )
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              width: 2
+                          )
+                      ),
+                      prefixIcon: IconButton(
+                        icon: Icon(Icons.email),
+                        onPressed:(){},
+                      ),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.deepPurple,
-                            width: 2
-                        )
-                    ),
-                    prefixIcon: IconButton(
-                      icon: Icon(Icons.email),
-                      onPressed:(){},
-                    ),
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return'Please enter your email';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value){
-                    if(value!.isEmpty){
-                      return'Please enter your email';
-                    }
-                    return null;
-                  },
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: _phone,
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                    labelText: 'phone_no',
-                    hintText: 'Enter phone number',
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.orange,
-                            width: 2
-                        )
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _phone,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      labelText: 'phone_no',
+                      hintText: 'Enter phone number',
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.orange,
+                              width: 2
+                          )
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              width: 2
+                          )
+                      ),
+                      prefixIcon: IconButton(
+                        icon: Icon(Icons.phone),
+                        onPressed: (){},
+                      ),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.deepPurple,
-                            width: 2
-                        )
-                    ),
-                    prefixIcon: IconButton(
-                      icon: Icon(Icons.phone),
-                      onPressed: (){},
-                    ),
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return'Please enter your phone number';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value){
-                    if(value!.isEmpty){
-                      return'Please enter your phone number';
-                    }
-                    return null;
-                  },
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  controller: _pass,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'password',
-                    hintText: 'Enter Password',
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.orange,
-                            width: 2
-                        )
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: _pass,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'password',
+                      hintText: 'Enter Password',
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.orange,
+                              width: 2
+                          )
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                              color: Colors.deepPurple,
+                              width: 2
+                          )
+                      ),
+                      prefixIcon: IconButton(
+                        icon: Icon(Icons.key),
+                        onPressed: (){},
+                      ),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(
-                            color: Colors.deepPurple,
-                            width: 2
-                        )
-                    ),
-                    prefixIcon: IconButton(
-                      icon: Icon(Icons.key),
-                      onPressed: (){},
-                    ),
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return'Please enter your password';
+                      }
+                      return null;
+                    },
                   ),
-                  validator: (value){
-                    if(value!.isEmpty){
-                      return'Please enter your password';
-                    }
-                    return null;
-                  },
                 ),
-              ),
-              Center(
-                child: ElevatedButton(onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginDetail(name: _uname.text, email: _email.text, phone: _phone.text, password: _pass.text)),
-                  );
-                }, child: Text('Login')),
-              )
-            ]),
+                Center(
+                  child: ElevatedButton(onPressed: (){
+
+                    if(_formKey.currentState!.validate())
+                      {
+                        DataModel datamodel = DataModel(
+                            name: _uname.text,
+                            email: _email.text,
+                            phone: _phone.text,
+                            password: _pass.text);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginDetails(dataModel1: datamodel),
+                          ),
+                        );
+                      }
+
+
+                  }, child: Text('Login')),
+                )
+              ]),
             )
-          ),
         ),
       ),
     );
   }
 }
+
